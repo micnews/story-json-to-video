94ebb447bfc8efc446191aaac353e2ef064fba14
Serge <shayief@gmail.com>
2018-02-01 22:06:04 +0200

    Read json file and concat videos and images together

---
+++ bin/cli.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const articleJsonToVideo = require('../lib/main');
+
+function usage() {
+  console.log('USAGE: article-json-to-video <story.json>');
+  process.exit(1);
+}
+
+const args = process.argv.slice(2);
+if (!args[0]) {
+  usage();
+}
+
+const json = JSON.parse(fs.readFileSync(args[0], 'utf8'));
+
+articleJsonToVideo(json)
+  .catch(err => console.log(err));
