a5c94f2a4d70d7bf6b3b3ef0cdd37edc0b68d6f9
Serge <shayief@gmail.com>
2018-02-15 12:03:51 -0500

    Custom fonts and improved logging

--- bin/cli.js
+++ bin/cli.js
@@ -1,18 +1,51 @@
 #!/usr/bin/env node
 
+const argv = require('minimist')(process.argv.slice(2), {
+  string: ['font'],
+});
 const fs = require('fs');
 const articleJsonToVideo = require('../lib/main');
 
+const fontOpts = (Array.isArray(argv.font) ? argv.font : [argv.font]).filter(Boolean);
+const fonts = fontOpts.map((fontStr) => {
+  const [family, source] = fontStr.split('=');
+  try {
+    fs.statSync(source);
+  } catch (e) {
+    console.error(`Unable to load font ${source}`);
+    process.exit(1);
+  }
+
+  return { family, source };
+});
+
 function usage() {
-  console.log('USAGE: article-json-to-video <story.json>');
+  console.log('USAGE: article-json-to-video [options] <story.json>');
+  console.log('');
+  console.log('OPTIONS:');
+  console.log('  --font <family>=<source-file>     Use custom font from the file');
+  console.log('  --print-ffmpeg                    Print ffmpeg output');
   process.exit(1);
 }
 
-const args = process.argv.slice(2);
-if (!args[0]) {
+if (argv.help) {
   usage();
 }
 
-const json = JSON.parse(fs.readFileSync(args[0], 'utf8'));
+const input = argv._[0];
+if (!input) {
+  usage();
+}
 
-articleJsonToVideo(json).catch(err => console.log(err));
+let json;
+try {
+  json = JSON.parse(fs.readFileSync(input, 'utf8'));
+} catch (e) {
+  console.error(`Unable to load json ${input}`);
+  process.exit(1);
+}
+
+articleJsonToVideo(json, {
+  fonts,
+  printFfmpeg: argv['print-ffmpeg'],
+}).catch(err => console.error('Unexpected error', err));
